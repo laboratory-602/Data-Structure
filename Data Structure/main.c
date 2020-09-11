0d23553744c8abcd5064166e204f630f33140ee7
Derek <chenjunqi07@icloud.com>
2020-09-11 20:47:37 +0800

    Initial Commit

---
+++ Data Structure/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Data Structure
+//
+//  Created by Derek Chan on 2020/9/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
